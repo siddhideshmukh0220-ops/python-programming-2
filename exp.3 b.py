e335392d75a8f68cffc2fb0e745a2a2f14e0fb09
siddhideshmukh0220-ops <siddhideshmukh0220@gmail.com>
2026-03-20 13:32:24 +0530

    Add files via upload

---
+++ exp.3 b.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 20 13:30:46 2026
+
+@author: User
+"""
+
+n = int(input("Enter number of rows: "))
+for i in range(1, n + 1):
+ for j in range(1, i + 1):
+  print(j, end=" ")
+print()
