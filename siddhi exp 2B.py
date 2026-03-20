9bbf2c7ccad5644d134dce10fb1e1d8dac3de6c0
siddhideshmukh0220-ops <siddhideshmukh0220@gmail.com>
2026-03-20 13:27:44 +0530

    Add files via upload

---
+++ siddhi exp 2B.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 20 13:11:55 2026
+
+@author: User
+"""
+def simple_interest(principal, rate, time):
+ si = (principal * rate * time) / 100
+ return si
+# Taking input from the user
+p = float(input("Enter principal amount: "))
+r = float(input("Enter rate of interest: "))
+t = float(input("Enter time (in years): "))
+# Function call
+interest = simple_interest(p, r, t)
+print("Simple Interest is:", interest)
